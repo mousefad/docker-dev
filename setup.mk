daed1f8838046ceff2a28eca1528b0506ff20566
Mouse <mousefad@googlemail.com>
2025-07-18 23:49:22 +0100

    share gitlab ssh port to host

--- setup.mk
+++ setup.mk
@@ -4,8 +4,9 @@ TZ = Europe/London
 SSH_PUB_KEY = ~/.ssh/local.pub
 REGULAR_USER = bob
 PORT_HTTP_FOSSIL = 42421
-PORT_GITLAB_HTTP = 42422
-PORT_GITLAB_HTTPS = 42423
+PORT_GITLAB_SSH = 42422
+PORT_GITLAB_HTTP = 42423
+PORT_GITLAB_HTTPS = 42424
 
 # choose from docker, podman etc...
 DOCKER = docker
